0469d765ccdfa723bf9e6da70b22ac5d2d20178f
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:46:38 +0100

    Create intDouble.java

---
+++ type conversion/intDouble.java
@@ -0,0 +1,16 @@
+class intDouble {
+  public static void main(String[] args) {
+
+    // create int variables
+    int a =33;
+    int b = 29;
+
+    // convert int into double
+    // using typecasting
+    double c = a;
+    double d = b;
+
+    System.out.println(c);    // 33.0
+    System.out.println(d);    // 29.0
+  }
+}
