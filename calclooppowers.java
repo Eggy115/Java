b362035f4a9c6f7d3c37762bec7b7067959e9221
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:20:08 +0100

    Create calclooppowers.java

---
+++ calclooppowers.java
@@ -0,0 +1,14 @@
+class calclooppowers {
+  public static void main(String[] args) {
+
+    int base = 3, exponent = 4;
+
+    long result = 1;
+
+    for (; exponent != 0; --exponent) {
+      result *= base;
+    }
+
+    System.out.println("Answer = " + result);
+  }
+}
