ad165853a7c90fe9865518af4bb7f68e7b33b7b3
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:24:23 +0100

    Create printing.java

---
+++ printing.java
@@ -0,0 +1,20 @@
+public class printing {
+    public static void main(String args[]) {
+        // Print without newline
+        System.out.print("Eggy115");
+
+        // Print with newline
+        System.out.println("Eggy115");
+        System.out.print("Eggy115\n");
+        
+        // Print variable
+        int num1 = 10;
+        System.out.println(num1);
+        
+        // Print variable with string
+        int num2 = 20;
+        System.out.println("Number = " + num2);
+
+        System.out.println("Number1 = " + num1 + "\n" + "Number2 = " + num2);
+    }
+}
