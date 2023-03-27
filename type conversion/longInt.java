a4602f10e30eb07fad3d3077ae4ea491940d86bf
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:43:49 +0100

    Create longInt.java

---
+++ type conversion/longInt.java
@@ -0,0 +1,13 @@
+class longInt {
+  public static void main(String[] args) {
+
+    // create an object of Long class
+    Long obj = 52341241L;
+
+    // convert object of Long into int
+    // using intValue()
+    int a = obj.intValue();
+
+    System.out.println(a);    // 52341241
+  }
+}
