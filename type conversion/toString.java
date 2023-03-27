963ae9e5ed10ab15c89b18a33401035a27eb4b36
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:47:53 +0100

    Create toString.java

---
+++ type conversion/toString.java
@@ -0,0 +1,17 @@
+class toString {
+  public static void main(String[] args) {
+
+    // create double variables
+    double num1 = 4.76;
+    double num2 = 786.56;
+
+    // convert double to string
+    // using toString()
+    String str1 = Double.toString(num1);
+    String str2 = Double.toString(num2);
+
+    // print string variables
+    System.out.println(str1);    // 4.76
+    System.out.println(str2);    // 786.56
+  }
+}
