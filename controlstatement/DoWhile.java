9894c26e546d4855ccdc5fc150a16e74138c9c4a
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:32:07 +0100

    Add files via upload

---
+++ controlstatement/DoWhile.java
@@ -0,0 +1,7 @@
+public class DoWhile {  
+    public static void main(String[] args) {  
+        do{  
+            System.out.println("infinitive do while loop");  
+        }while(true);  
+    }  
+    } 
---
+++ controlstatement/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {    
+    public static void main(String[] args) {   
+     // setting the infinite while loop by passing true to the condition  
+        while(true){    
+            System.out.println("infinitive while loop");    
+        }    
+    }    
+    } 
+
+    
