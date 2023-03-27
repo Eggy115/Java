75c709e977ab10cd09418202f69459281c772144
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:40:23 +0100

    Create CharStringArray.java

---
+++ type conversion/CharStringArray.java
@@ -0,0 +1,12 @@
+public class CharStringArray {
+
+    public static void main(String[] args) {
+        char[] ch = {'a', 'e', 'i', 'o', 'u'};
+
+        String st = String.valueOf(ch);
+        String st2 = new String(ch);
+
+        System.out.println(st);
+        System.out.println(st2);
+    }
+}
