91109fc3ad733efd2f420bb0e197ca32ba3659c1
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:46:53 +0100

    Create intDoublevalueOf.java

---
+++ type conversion/intDoublevalueOf.java
@@ -0,0 +1,13 @@
+class intDoublevalueOf {
+  public static void main(String[] args) {
+
+    // create int variables
+    int a = 332;
+
+    // convert to an object of Double
+    // using valueOf()
+    Double obj = Double.valueOf(a);
+
+    System.out.println(obj);    // 332.0
+  }
+}
