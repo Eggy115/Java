cf734e009139aca723e3551e6908b11ce99bd512
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:45:55 +0100

    Create parseint.java

---
+++ type conversion/parseint.java
@@ -0,0 +1,17 @@
+class parseint {
+  public static void main(String[] args) {
+
+    // create string variables
+    String str1 = "23";
+    String str2 = "4566";
+
+    // convert string to int
+    // using parseInt()
+    int num1 = Integer.parseInt(str1);
+    int num2 = Integer.parseInt(str2);
+
+    // print int values
+    System.out.println(num1);    // 23
+    System.out.println(num2);    // 4566
+  }
+}
