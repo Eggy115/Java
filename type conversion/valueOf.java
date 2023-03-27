150d6382626e9f0eb91f56c405fa5d049304a571
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-27 18:44:23 +0100

    Create valueOf.java

---
+++ type conversion/valueOf.java
@@ -0,0 +1,13 @@
+class valueOf {
+  public static void main(String[] args) {
+
+    // create int variables
+    int a = 251;
+
+    // convert to an object of Long
+    // using valueOf()
+    Long obj = Long.valueOf(a);
+
+    System.out.println(obj);    // 251
+  }
+}
